f7b94bc43a561b58939fc112a89d665efda682c6
artur oliviera <ao.ragazzi@gmail.com>
2025-06-07 00:15:02 -0300

    Finalizado modulo Fundamentos

---
+++ 01 - Fundamentos/Arrays/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Arrays{
+    class  Program{
+        
+        static string[] agenda = new string[10]; 
+        
+        static void Main(){
+            Console.WriteLine(agenda.Length);
+            Menu();
+        }
+
+
+        static void Menu(){
+            Console.Clear();
+            int escolha;
+            do{
+                Console.WriteLine("1 - Adicionar Contato\n" +
+                                  "2 - Remover contato\n" +
+                                  "3 - Editar contato\n" +
+                                  "4 - Listar contatos\n" +
+                                  "5 - Popular contatos\n" +
+                                  "0 - Sair");
+                escolha = Convert.ToInt32(Console.ReadLine());
+
+                switch (escolha){
+                    case 1: Adicionar(); break;
+                    case 2: Remover(); break;
+                    case 3: Editar(); break;
+                    case 4: Listar(); break;
+                    case 5: Popular(); break;
+                }
+            } while (escolha != 0);
+
+        }
+
+        static void Adicionar(){
+            Console.Clear();
+            Console.WriteLine("Digite a posicao do contato: ");
+            int posicao = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o nome do contato");
+            agenda[posicao] = Console.ReadLine();
+            
+            
+        }
+        static void Remover(){
+            Console.Clear();
+            Console.WriteLine("Digite a posicao do contato: ");
+            int posicao = Convert.ToInt32(Console.ReadLine());
+            agenda[posicao] = null;
+            
+        }
+        static void Editar(){
+            Console.Clear();
+            Remover();
+            Adicionar();
+        }
+
+        static void Listar(){
+            Console.Clear();
+            for (int i = 0; i < agenda.Length; i++){
+                Console.WriteLine(agenda[i]);
+            }
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Popular(){
+            for (int i = 0; i < agenda.Length; i++){
+                agenda[i] = ($"Joazinho da Silva {i}");
+            }
+        }
+    }
+}
---
+++ 01 - Fundamentos/Moedas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Moeda
+{
+class Program{
+    static void Main(string[] args){
+        
+        Console.Clear();
+        decimal valor = 12520.25m;
+        Console.WriteLine(valor);
+        
+        //Formatando Moedas
+        
+        Console.WriteLine(valor.ToString("C",CultureInfo.CreateSpecificCulture("en-US")));
+        Console.WriteLine(valor.ToString("C",CultureInfo.CreateSpecificCulture("pt-BR")));
+        Console.WriteLine(valor.ToString("C",CultureInfo.CreateSpecificCulture("pt-PT")));
+        
+    }
+}
+}
