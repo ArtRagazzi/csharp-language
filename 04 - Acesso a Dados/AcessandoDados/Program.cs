cdea0cb2b047584c076ef814f58449a0b45f6a83
artur oliviera <ao.ragazzi@gmail.com>
2025-06-17 14:08:21 -0300

    Executando Procedure

--- 04 - Acesso a Dados/AcessandoDados/Program.cs
+++ 04 - Acesso a Dados/AcessandoDados/Program.cs
@@ -33,9 +33,12 @@ namespace AcessandoDados{
             //Usando Dapper
             //Nao ficar gerando codigo dentro da Sessao aberta
             using (var conn = new SqlConnection(databaseConnection)){
-                InsertCategory(conn);
-                UpdateCategory(conn);
+                //InsertCategory(conn);
+                //InsertManyCategory(conn);
+                //UpdateCategory(conn);
                 ListCategories(conn);
+                //DeleteCategory(conn,new Guid("84614f7e-31ee-4f23-ba76-8273f368c8cf"));
+                ExecuteProcedure(conn);
             }
             
         }
@@ -64,12 +67,59 @@ namespace AcessandoDados{
                     featured = categoryToInsert.Featured
                 });
         }
+        
+        static void InsertManyCategory(SqlConnection connection){
+            var categoryToInsert = new Category("Amazon AWS1", "amazon1", 8, "AWS Cloud1",
+                "Categoria destinada a serviços da AWS1",false);
+            
+            var categoryToInsert2 = new Category("Amazon AWS2", "amazon2", 8, "AWS Cloud2",
+                "Categoria destinada a serviços da AWS2",true);
+
+            var insertSQL = "INSERT INTO Category(Id, Title, Url, [Order], Summary, Description, Featured) VALUES (@id, @title, @url, @order, @summary, @description, @featured)";
+
+            var rows = connection.Execute(insertSQL, new[]{
+                    new 
+                    {
+                        id = categoryToInsert.Id,
+                        title = categoryToInsert.Title,
+                        url = categoryToInsert.Url,
+                        order = categoryToInsert.Order,
+                        summary = categoryToInsert.Summary,
+                        description = categoryToInsert.Description,
+                        featured = categoryToInsert.Featured
+                    },
+                    new 
+                    { 
+                        id = categoryToInsert2.Id,
+                        title = categoryToInsert2.Title,
+                        url = categoryToInsert2.Url,
+                        order = categoryToInsert2.Order,
+                        summary = categoryToInsert2.Summary,
+                        description = categoryToInsert2.Description,
+                        featured = categoryToInsert2.Featured
+                    }
+                });
+        }
 
         static void UpdateCategory(SqlConnection connection){
             var updateQuery = "UPDATE Category SET Title=@title WHERE Id=@id";
             var rows = connection.Execute(updateQuery,
                 new{ id = "84614f7e-31ee-4f23-ba76-8273f368c8cf", title = "AWS 2025" });
             Console.WriteLine("Linhas Atualizadas: "+ rows);
+        }
+        
+        static void DeleteCategory(SqlConnection connection, Guid id){
+            var deleteQuery = "DELETE FROM Category WHERE Id = @id";
+            var rows = connection.Execute(deleteQuery, new{ id });
+            Console.WriteLine("Linhas excludias "+rows);
+        }
+
+        static void ExecuteProcedure(SqlConnection connection){
+            var procedure = "spDeleteStudent";
+            var par = new{ StudentId = "5404e368-1084-4289-b371-4a4851529863" };
+            
+            var rows =connection.Execute(procedure, par, commandType: System.Data.CommandType.StoredProcedure);
+            Console.WriteLine("Linhas afetadas: "+rows);
         }
     }
 }
