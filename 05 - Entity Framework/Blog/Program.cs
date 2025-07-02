00d2da8dd85b9e056a0c4e7b46c860adaa2a7e60
artur oliviera <ao.ragazzi@gmail.com>
2025-07-02 18:22:24 -0300

    Finalizando Introdução

--- 05 - Entity Framework/Blog/Program.cs
+++ 05 - Entity Framework/Blog/Program.cs
@@ -1,8 +1,43 @@
-﻿namespace Blog
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
 {
     class Program{
         static void Main(string[] args){
-            Console.WriteLine();
+            using (var context = new BlogDataContext()){
+                /* Insert
+                var tag = new Tag{ Name = "ASP.NET", Slug = "aspnet" };
+                context.Tags.Add(tag);
+                */
+                /*Update
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                tag.Name = ".NET";
+                context.Update(tag);
+                */
+                /*Delete
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                context.Remove(tag);
+                */
+                /*ToList
+                //ToList sempre no Final (USE ASNOTRACKING para consultas full, somente para Selects
+                var tags = context
+                    .Tags
+                    .Where(x=> x.Name.Contains("Java"))
+                    .ToList();
+                foreach (var tag in tags){
+                    Console.WriteLine($"{tag.Name}");
+                }
+                */
+                var tag = context.Tags
+                    .AsNoTracking()
+                    .FirstOrDefault(x=>x.Id==3);
+
+                Console.WriteLine($"{tag?.Name}");
+                //Salva o que esta no Context (Memoria) No Banco
+                context.SaveChanges();
+            }
         }
     }
 }
--- 05 - Entity Framework/Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ 05 - Entity Framework/Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9065ca7bff0bdbb5e2fbadfeef952f835d8979b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6925f65506dce7faa93da39bdf53dc4938d44c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
