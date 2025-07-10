ee1c499ee1d503162e094992496c64164ec7accd
Artur Ragazzi <ao.ragazzi@gmail.com>
2025-07-10 16:27:33 -0300

    Final Crud Exceptions 12

--- 06 - Fundamentos ASPNET/BlogCrudEF/Controllers/CategoryController.cs
+++ 06 - Fundamentos ASPNET/BlogCrudEF/Controllers/CategoryController.cs
@@ -20,55 +20,84 @@ public class CategoryController : ControllerBase{
     
     [HttpGet("v1/categories/{id:int}")]
     public async Task<IActionResult> GetByIdAsync([FromServices]BlogDataContext context, [FromRoute] int id){
-        
-        var category = await context.Categories.FirstOrDefaultAsync(x=> x.Id == id);
-        if (category == null){
-            return NotFound();
+        try{
+            
+            var category = await context.Categories.FirstOrDefaultAsync(x=> x.Id == id);
+            if (category == null){
+                return NotFound();
+            }
+            return Ok(category);
+        }catch (DbUpdateException e){
+            return StatusCode(500, e.Message);
         }
-        return Ok(category);
+        catch (Exception e){
+            return StatusCode(500, "Internal server error");
+        }
         
     }
     
     
     [HttpPost("v1/categories")]
     public async Task<IActionResult> InsertAsync([FromServices]BlogDataContext context, [FromBody] Category category){
-        
-        await context.Categories.AddAsync(category);
-        await context.SaveChangesAsync();
-        
-        return Created($"v1/categories/{category.Id}", category);
+
+        try{
+            await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+            return Created($"v1/categories/{category.Id}", category);
+        }
+        catch (DbUpdateException e){
+            return StatusCode(500, e.Message);
+        }
+        catch (Exception e){
+            return StatusCode(500, "Internal server error");
+        }
         
     }
     
     [HttpPut("v1/categories/{id:int}")]
     public async Task<IActionResult> UpdateAsync([FromServices]BlogDataContext context, [FromBody] Category category, [FromRoute] int id){
-        
-        var categoryToUpdate = await context.Categories.FirstOrDefaultAsync(x=> x.Id == id);
-        if (categoryToUpdate == null){
-            return NotFound();
+
+        try{
+            
+            var categoryToUpdate = await context.Categories.FirstOrDefaultAsync(x=> x.Id == id);
+            if (categoryToUpdate == null){
+                return NotFound();
+            }
+            
+            categoryToUpdate.Name = category.Name;
+            categoryToUpdate.Slug = category.Slug;
+            
+            context.Categories.Update(categoryToUpdate);
+            await context.SaveChangesAsync();
+            
+            return Ok(categoryToUpdate);
+        }catch (DbUpdateException e){
+            return StatusCode(500, e.Message);
         }
-        
-        categoryToUpdate.Name = category.Name;
-        categoryToUpdate.Slug = category.Slug;
-        
-        context.Categories.Update(categoryToUpdate);
-        await context.SaveChangesAsync();
-        
-        return Ok(categoryToUpdate);
+        catch (Exception e){
+            return StatusCode(500, "Internal server error");
+        }
         
     }
     
     [HttpDelete("v1/categories/{id:int}")]
     public async Task<IActionResult> DeleteAsync([FromServices]BlogDataContext context, [FromRoute] int id){
-        
-        var categoryToDelete = await context.Categories.FirstOrDefaultAsync(x=> x.Id == id);
-        if (categoryToDelete == null){
-            return NotFound();
+        try{
+            var categoryToDelete = await context.Categories.FirstOrDefaultAsync(x=> x.Id == id);
+            if (categoryToDelete == null){
+                return NotFound();
+            }
+            
+            context.Categories.Remove(categoryToDelete);
+            await context.SaveChangesAsync();
+            return Ok(categoryToDelete);
+            
+        }catch (DbUpdateException e){
+            return StatusCode(500, e.Message);
         }
-        
-        context.Categories.Remove(categoryToDelete);
-        await context.SaveChangesAsync();
-        return Ok(categoryToDelete);
+        catch (Exception e){
+            return StatusCode(500, "Internal server error");
+        }
         
     }
 }
