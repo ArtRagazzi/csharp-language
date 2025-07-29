d244c1d4ddf97c0f729c95f4d907f7ffb20eec02
Artur O <ao.ragazzi@gmail.com>
2025-07-29 09:10:05 -0300

    Agora é no repo MoneyExtension

---
+++ 07 - Fundamentos Azure, Git e DevOps/MoneyExtension/MoneyExtension/Money.cs
@@ -0,0 +1,20 @@
+﻿namespace MoneyExtension;
+
+public static class Money
+{
+    
+    public static int ToCents(this decimal amout){
+        if(amout <= 0){
+            return 0;
+        }
+
+        var text = amout.ToString("N2").Replace(",","").Replace(".",",");
+        if(string.IsNullOrEmpty(text)){
+            return 0;
+        }
+
+        int.TryParse(text, out var result);
+        return result;
+    }
+
+}
