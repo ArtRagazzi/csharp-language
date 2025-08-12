27a9ecdbfd22f955923877be8b157e279a2e0bf0
Artur O <ao.ragazzi@gmail.com>
2025-08-12 11:38:23 -0300

    Finalizando 07 - Definindo Entidades

---
+++ 08 - Modelando Dominios Ricos/PaymentContext/PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,12 @@
+namespace PaymentContext.Domain.Entities;
+
+
+public class Subscription{
+    public bool Active {get; set;}
+    public DateTime CreateDate {get; set;}
+    public DateTime LastUpdateDate {get; set;}
+    public DateTime? ExpireDate {get; set;}
+
+
+    public List<Payment> Payments { get; set; }
+}
