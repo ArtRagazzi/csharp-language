f780670db381198537473958a9f90f6d02b4de9f
Artragazzi <ao.ragazzi@gmail.com>
2025-09-01 19:40:22 -0300

    Ollama exemple

---
+++ Projetos/Ollama-dotnet/Program.cs
@@ -0,0 +1,26 @@
+﻿using OllamaSharp;
+
+var url = new Uri("http://localhost:11434/");
+var client = new OllamaApiClient(url);
+
+var  models = await client.ListLocalModelsAsync();
+// foreach (var model in models)
+// {
+//     Console.WriteLine(model.Name);
+// }
+
+client.SelectedModel = "llama3.2:latest";
+var chat = new Chat(client);
+
+Console.WriteLine("Qual a sua pergunta/duvida?");
+var pergunta = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("Que tipo de humor você quer que eu responda? EX. Engraçado, Triste, Nervoso....");
+var humor = Console.ReadLine() ?? "Normal";
+
+var prompt = $"Responda a pergunta {pergunta} de forma/humor {humor}, quero que reforce o humor nas respostas, não quero que mostre a mensagem de confirmação, va direto para a resposta";
+Console.Clear();
+await foreach (var awnser in chat.SendAsync(prompt))
+{
+    Console.Write(awnser);
+}
